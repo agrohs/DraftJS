ecb151e1dbefd34f4fc4582a419caf2abb0383b5
Adam Grohs <agrohs@users.noreply.github.com>
2021-06-24 07:20:14 -0500

    initial commit

--- src/components/Agnoponent/Agnoponent.style.js
+++ src/components/Agnoponent/Agnoponent.style.js
@@ -3,8 +3,12 @@ import styled from 'styled-components'
 export const BlockWrapper = styled.span.attrs(() => ({
   editable: false,
 }))`
-  && {
-    cursor: pointer;
-    user-select: none;
-  }
+  ${({ theme: { space } }) => `
+    margin: 0 1px;
+
+    && > * {
+      cursor: pointer;
+      user-select: none;
+    }
+  `}
 `
