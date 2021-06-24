59d78c82d49ccb2a88c2d1df6d7dd9eaa3a0f7b6
Adam Grohs <adam@uniquelyparticular.com>
2021-06-24 18:10:10 -0500

    chore: WIP

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Agnoponent } from './Agnoponent/Agnoponent'
 export { default as MacroEditor } from './MacroEditor/MacroEditor'
 export { default as Macros } from './Macros/Macros'
+export { default as MentionSuggestion } from './MentionSuggestion/MentionSuggestion'
